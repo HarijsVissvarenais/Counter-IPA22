c5d4083a4b893e688244c8c281ffc0bb9fbfda70
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 11:36:47 +0300

    shivers

---
+++ src/ToDo.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+function ToDo() {
+    // const [todo, setTodo] = useState(todo);
+    return(
+        <>
+ <h1>Markuss ir OJ</h1>
+        </>
+    );
+}
+export default ToDo;
